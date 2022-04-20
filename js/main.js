29dcef695aa193362878c93080235b674dde4cd5
kinmokusei404 <b.s.10074@gmail.com>
2022-04-20 19:42:22 +0900

    変更内容 最新のつぶやきが最上段に来るように変更 新規作成ボタンを追加 いいねボタン押下時にカウントアップされ、、画面上に総数が表示されるように変更

--- js/main.js
+++ js/main.js
@@ -2,30 +2,32 @@ const app = Vue.createApp({
   data: () => ({
     tweets: []
   }),
+  mounted: function() {
+    console.log('Clicked!')
+    fetch('http://localhost:3000/POST', {
+      method: 'GET'
+    })
+    .then(response => {
+      console.log(response);
+      return response.json();
+    })
+    .then(data => {
+      var n = 0;
+      for(var i=data.length-1; i>=0; i--){
+        let tweet = {
+          soeji: n,
+          post_id: data[i].id,
+          post_user_id: data[i].POST_USER_ID,
+          post_datetime: data[i].POST_DATETIME,
+          post_text: data[i].POST_TEXT,
+          post_fav_cnt: data[i].POST_FAV_CNT
+        };
+        this.tweets.push(tweet);
+        n++;
+      }
+    });
+  },
   methods: {
-    selectPost: function() {
-      console.log('Clicked!')
-      fetch('http://localhost:3000/POST', {
-        method: 'GET'
-      })
-      .then(response => {
-        console.log(response);
-        return response.json();
-      })
-      .then(data => {
-        console.log(data[0].id);
-        for(var i=0; i<data.length; i++){
-          let tweet = {
-            post_id: data[i].id,
-            post_user_id: data[i].POST_USER_ID,
-            post_datetime: data[i].POST_DATETIME,
-            post_text: data[i].POST_TEXT,
-            post_fav_cnt: data[i].POST_FAV_CNT
-          };
-          this.tweets.push(tweet);
-        }
-      });
-    },
     // FAVORITEテーブルの登録
     createFavorite: function(n) {
       console.log('Clicked!' + n);
@@ -35,21 +37,21 @@ const app = Vue.createApp({
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          'POST_ID': this.tweets[n-1].post_id,
-          'USER_ID': this.tweets[n-1].post_user_id
+          'POST_ID': this.tweets[n].post_id,
+          'USER_ID': this.tweets[n].post_user_id
         })
       });
-      console.log(this.tweets[n-1].post_fav_cnt);
+      console.log(this.tweets[n].post_fav_cnt);
       // いいねカウント数のインクリメント
-      this.tweets[n-1].post_fav_cnt += 1
+      this.tweets[n].post_fav_cnt += 1
       // POSTテーブルの「いいねカウント数」の更新
-      fetch('http://localhost:3000/POST/' + n, {
+      fetch('http://localhost:3000/POST/' + this.tweets[n].post_id, {
         method: 'PATCH',
         headers: {
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-          'POST_FAV_CNT': this.tweets[n-1].post_fav_cnt
+          'POST_FAV_CNT': this.tweets[n].post_fav_cnt
         })
       });
     }
