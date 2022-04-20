55a8fa4cc8c86aff85766c3b3146561d13e3338e
kinmokusei404 <b.s.10074@gmail.com>
2022-04-20 09:34:08 +0900

    SearchAll作成

---
+++ src/js/searchAll.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue'
+
+const app = createApp(
+  fetch('http://localhost:3000/POST', {
+    method: 'GET'
+  })
+  .then(response => {
+    console.log(response)
+    return response.json();
+  })
+  .then(data => {
+    var text = document.getElementById('data');
+    for(var i=0; i < data.length;i++){
+      text.innerHTML += `${data[i].POST_USER_ID} ${data[i].POST_DATETIME}  <br> ${data[i].POST_TEXT} <br> <button>いいね</button> <br> <br>`;
+    }
+  })
+);
+
+app.mount('#searchall');
+
+export default {
+  name: 'App',
+};
