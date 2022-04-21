2561f836cf4e74a7eef24c5899f60e93b7d285dd
isoh45 <isobe.hirotaka.h45@jp.nssol.nipponsteel.com>
2022-04-21 16:30:14 +0900

    ツイートを編集

--- js/main.js
+++ js/main.js
@@ -13,47 +13,64 @@ const app = Vue.createApp({
     })
     .then(data => {
       var n = 0;
-      var tweetListPre = [];
-      var tweetListCil = [];
-      for(var i=0; i<data.length; i++){
-        if(data[i].REPLY_POST_ID == null){
-          tweetListPre.push(data[i]);
-        }else{
-          tweetListCil.push(data[i]);
-        }
-      }
-      // console.log(tweetListPre.length)
-      // console.log(tweetListCil.length)
-      for(var i=tweetListPre.length-1; i>=0; i--){
+      for(var i=data.length-1; i>=0; i--){
         let tweet = {
           soeji: n,
-          post_id: tweetListPre[i].id,
-          post_user_id: tweetListPre[i].POST_USER_ID,
-          post_datetime: tweetListPre[i].POST_DATETIME,
-          post_text: tweetListPre[i].POST_TEXT,
-          post_fav_cnt: tweetListPre[i].POST_FAV_CNT,
-          url: "twitCreate.html?id="+tweetListPre[i].id
+          post_id: data[i].id,
+          post_user_id: data[i].POST_USER_ID,
+          post_datetime: data[i].POST_DATETIME,
+          post_text: data[i].POST_TEXT,
+          post_fav_cnt: data[i].POST_FAV_CNT,
+          is_edit_mode: false,
+          post_text_bef_edit: data[i].POST_TEXT,
+          message: ''
         };
         this.tweets.push(tweet);
         n++;
-        // for(var n=0; n<tweetListCil.length; n++){
-        //   if(tweetListCil[n].REPLY_POST_ID == tweetListPre[i].id){
-        //     let tweet = {
-        //       soeji: n,
-        //       post_id: tweetListCil[n].id,
-        //       post_user_id: tweetListCil[n].POST_USER_ID,
-        //       post_datetime: tweetListCil[n].POST_DATETIME,
-        //       post_text: tweetListCil[n].POST_TEXT,
-        //       post_fav_cnt: tweetListCil[n].POST_FAV_CNT
-        //     };
-        //     this.tweets.push(tweet);
-        //     n++;
-        //   }
-        // }
       }
     });
   },
   methods: {
+    // 編集モードのオンオフ切替
+    changeMode: function(n){
+      // 編集モードをオフ→オン
+      this.tweets[n].is_edit_mode = !(this.tweets[n].is_edit_mode)
+    },
+    // 編集モードをやめる
+    quitEdit: function(n){
+      // テキストエリアに編集前の本文を格納
+      this.tweets[n].post_text = this.tweets[n].post_text_bef_edit
+      // エラーメッセージをクリア
+      this.tweets[n].message = ''
+      // 編集モードをオン→オフ
+      this.tweets[n].is_edit_mode = !(this.tweets[n].is_edit_mode)
+    },
+    // ツイート本文の更新
+    updateText: function(n) {
+      // 文字数チェック
+      if(this.tweets[n].post_text.length > 140){
+        this.tweets[n].message = '140文字以内で入力してください'
+        return
+      }
+      if(this.tweets[n].post_text.length == 0){
+          this.tweets[n].message = '文字数0の状態で投稿できません'
+          return
+      }
+      // ツイート本文の更新
+      fetch('http://localhost:3000/POST/' + this.tweets[n].post_id, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'POST_TEXT': this.tweets[n].post_text
+        })
+      });
+      // エラーメッセージをクリア
+      this.tweets[n].message = ''
+      // 編集モードをオン→オフ
+      this.tweets[n].is_edit_mode = !(this.tweets[n].is_edit_mode)
+    },
     // FAVORITEテーブルの登録
     createFavorite: function(n) {
       console.log('Clicked!' + n);
